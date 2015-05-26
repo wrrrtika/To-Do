0fef346e5b02eea5e7772a1e96a09e2f983c47dc
JON HOUSE <J_HOUSE974@YAHOO.CA>
2015-05-26 15:50:06 -0400

    new task form

--- app/controllers/todoo_controller.rb
+++ app/controllers/todoo_controller.rb
@@ -4,5 +4,26 @@ class TodooController < ApplicationController
 		@todone = Todoo.where(done: true)
 	end
 
-	
+	def new
+		@todo = Todoo.new
+	end
+
+	def todo_params
+		params.require(:todoo).permit(:name, :description, :done)
+	end
+
+	def create
+		@todo = Todoo.new(todo_params)
+
+		if @todo.save
+			redirect_to todoo_index_path, :notice => "Your item was created"
+		else
+			redirect_to todoo_index_path, :notice => "please try again"
+		end
+	end
+
+
+
+
+
 end
